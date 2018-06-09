0c4eece03316820175d8fee9990966ca41121662
yaniv ziperfal <ziperfal123@gmail.com>
2018-06-09 11:46:49 +0300

    integration
    starting basic integration

---
+++ includes/script.js
@@ -0,0 +1,24 @@
+// var submitButton = document.getElementById("theButton");
+// submitButton.onclick = function() {
+//     if () {
+//         swal ({
+//             title: "Thank You.",
+//             text: "Comment has been sent succesfully",
+//             icon: "success",
+//         });    
+//     }
+
+
+//     else {
+//         swal ({
+//             title: "Missing fields!",
+//             text: "Please fill the required fields.",
+//             icon: "error",
+//         });            
+//     }
+// }
+
+
+
+
+
