a1ffa2a3b9410a4765978791b4bc3137381ed436
yaniv ziperfal <ziperfal123@gmail.com>
2018-06-11 00:12:34 +0300

    2.3
    starting "media"...

--- includes/script.js
+++ includes/script.js
@@ -1,9 +1,11 @@
-swal({
-    title: "Good job!",
-    text: "Comment has been succesfully sent!",
-    icon: "success",
-  });
-  
+// swal({
+//     title: "Good job!",
+//     text: "Comment has been succesfully sent!",
+//     icon: "success",
+//   });
+//
+
+
 
 window.onload = function () {
 
@@ -12,6 +14,12 @@ window.onload = function () {
     var teams_list = document.getElementById("teams-list");
     var formation_list = document.getElementById("formation-list");
     var players_list = document.getElementById("players-list");
+    var formation1 = document.getElementById("formation3_5_2");
+    var formation2 = document.getElementById("formation4_3_3");
+    var dragItem = document.getElementById("dragElement");
+    var dropItem = document.getElementById("dropElement");
+
+
 
     document.getElementById("rival-team").onclick = function () {
         if (teams_list.style.display === "none") {
@@ -49,21 +57,63 @@ window.onload = function () {
 
     document.getElementById("subtitution").onclick = function () {
         players_list.style.display = "none";
-        teams_list.style.display = "block";
-        formation_list.style.display = "block";
+        teams_list.style.display = "none";
+        formation_list.style.display = "none";
     };
 
     document.getElementById("Comments").onclick = function () {
         players_list.style.display = "none";
-        teams_list.style.display = "block";
-        formation_list.style.display = "block";
+        teams_list.style.display = "none";
+        formation_list.style.display = "none";
     };
 
     document.getElementById("area-Select").onclick = function () {
         players_list.style.display = "none";
-        teams_list.style.display = "block";
-        formation_list.style.display = "block";
+        teams_list.style.display = "none";
+        formation_list.style.display = "none";
     };
+
+
+    document.getElementById("f2").onclick = function () {
+        if (formation1.style.display === "none") {
+            if (formation2.style.display = "block") {
+                formation2.style.display = "none";
+            }
+            formation1.style.display = "block";
+        } else {
+            formation1.style.display = "none";
+        }
+    }
+
+    document.getElementById("f1").onclick = function () {
+        if (formation2.style.display === "none") {
+            if (formation1.style.display = "block") {
+                formation1.style.display = "none";
+            }
+            formation2.style.display = "block";
+        } else {
+            formation2.style.display = "none";
+        }
+    }
+
+    // dropItem.ondragover = function (evt) {
+    //     evt.preventDefault();
+    // }
+    // dragItem.ondragstart = function (evt) {
+    //     evt.dataTransfer.setData('key', evt.target.id);
+    // }
+    //
+    //
+    // document.getElementById("dropElement").ondrop = function (evt) {
+    //     var dropElm = evt.dataTransfer.getData('key');
+    //     evt.preventDefault();
+    //     var myElement = document.getElementsByClassName("dropElement");
+    //     var myNewElement = document.createElement('img');
+    //     myNewElement.src = myElement.src;
+    //     dropItem.appendChild(myNewElement);
+    // }
+
+    // game clock //
 
     document.getElementById("startBtn").onclick = function ()  {
         status = 1;
@@ -87,7 +137,6 @@ window.onload = function () {
     };
 
 
-
     function timer() {
         if (status == 1) {
             setTimeout(function () {
@@ -108,5 +157,11 @@ window.onload = function () {
             }, 10);
         }
     }
+
+
+    document.getElementById("beitar").onclick = function () {
+        beitar.style.fontWeight = "bold";
+    };
+
 
 }
